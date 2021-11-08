8f4cdabe6c7ec219befc771590572d56c2624e71
thiago <thiagofreitascarneiro@hotmail.com>
2021-11-07 21:42:41 -0300

    creating function  to manipulate the tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,7 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
